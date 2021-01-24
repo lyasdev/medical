862cab6531fb5b5013bcf14243e1bce0ef09188f
s_bourahla <s_bourahla@esi.dz>
2021-01-24 12:59:02 +0100

    slim

--- patient2/api/src/main/java/com/lyaslim/medical/patient/api/App.java
+++ patient2/api/src/main/java/com/lyaslim/medical/patient/api/App.java
@@ -6,7 +6,7 @@ package com.lyaslim.medical.patient.api;
  */
 public class App 
 {
-    private String slimslim="";
+    private static String slimslim="";
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
