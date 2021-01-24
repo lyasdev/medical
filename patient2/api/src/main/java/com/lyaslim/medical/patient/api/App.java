6fb379ffdf7f717e12c1ddeebf5d0114a23e9497
s_bourahla <s_bourahla@esi.dz>
2021-01-24 14:58:59 +0100

    test

--- patient2/api/src/main/java/com/lyaslim/medical/patient/api/App.java
+++ patient2/api/src/main/java/com/lyaslim/medical/patient/api/App.java
@@ -6,10 +6,8 @@ package com.lyaslim.medical.patient.api;
  */
 public class App 
 {
-    private static String slimslim="";
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        slimslim="sliamne";
     }
 }
