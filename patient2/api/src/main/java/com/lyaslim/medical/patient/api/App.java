b43229f3b56b3d6cccdbf66e95c851f9a85c8e22
s_bourahla <s_bourahla@esi.dz>
2021-01-24 12:52:24 +0100

    String initialization

--- patient2/api/src/main/java/com/lyaslim/medical/patient/api/App.java
+++ patient2/api/src/main/java/com/lyaslim/medical/patient/api/App.java
@@ -6,7 +6,7 @@ package com.lyaslim.medical.patient.api;
  */
 public class App 
 {
-    private String slimslim;
+    private String slimslim="";
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
