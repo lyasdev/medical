186b04e1fe9a9ab76b68d62886f762979afea353
s_bourahla <s_bourahla@esi.dz>
2021-02-07 21:12:58 +0100

    Add patient details

--- patient/api/src/main/java/com/lyaslim/medical/patient/api/dtos/PatientDto.java
+++ patient/api/src/main/java/com/lyaslim/medical/patient/api/dtos/PatientDto.java
@@ -5,8 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 import com.lyaslim.medical.commons.api.dtos.DtoToEntityMapper;
-import com.lyaslim.medical.patient.domain.model.Patient;
-import com.lyaslim.medical.patient.domain.model.Sexe;
+import com.lyaslim.medical.patient.domain.model.*;
 import lombok.Data;
 import lombok.ToString;
 
@@ -16,16 +15,22 @@ public class PatientDto implements DtoToEntityMapper<Patient> {
 
     private Long id;
     private String firstName;
+    private String middleName;
     private String lastName;
     private String dateOfBirth;
     private Integer sexeId;
     private String sexeLabel;
-
+    private String identityCardNumber;
+    private Integer bloodGroupId;
     @Override
     public Patient toEntity() {
-        var dob  = Objects.isNull(dateOfBirth) ? null: LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        var dob  = Objects.isNull(dateOfBirth) ? null: LocalDate.parse(dateOfBirth,
+                DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         var sexe = Objects.isNull(sexeId) ? null : Sexe.from(sexeId);
-        return Patient.of(id,firstName,lastName,dob,
-                sexe);
+        var blood = Objects.isNull(bloodGroupId) ? null : BloodGroup.from(bloodGroupId);
+        Name name= Name.of(firstName, middleName,lastName);
+        Person person =Person.of(name,dob,sexe,blood,
+                identityCardNumber);
+        return Patient.of(id,person);
     }
 }
